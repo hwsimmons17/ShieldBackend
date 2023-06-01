20765e25dac522f85440328d143e82b349bb334e
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-05-31 21:04:55 -0600

    Test something...

--- app/__init__.py
+++ app/__init__.py
@@ -30,4 +30,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
