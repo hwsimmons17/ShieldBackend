e1c14c5320507945860ac4a922ba40a2ceb97261
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-05-31 21:01:58 -0600

    Change app initalization

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,8 @@ import uuid
 from flask import Flask, request
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    app.config["TEMPLATES_AUTO_RELOAD"] = True
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+def create_app():
+    app = Flask(__name__)
 
     # a simple page that says hello
     @app.route('/hello')
@@ -48,4 +30,4 @@ def create_app(test_config=None):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=os.getenv('PORT'))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
